6f2e8210866789acf98514f1d6d95638ccca5a2d
lifan6847 <lifan6847@navinfo.com>
2019-08-01 17:09:02 +0800

    第一次

---
+++ src/main/java/com/springstudy/springbootgitstudy/util/StringUtil.java
@@ -0,0 +1,11 @@
+package com.springstudy.springbootgitstudy.util;
+
+/**************************************
+ * Copyright (C), Navinfo
+ * Package: 
+ * @Author: lifan
+ * @Date: Created in 2019/8/1 13:00
+ * @Description:
+ **************************************/
+public class StringUtil {
+}
---
+++ src/test/java/com/springstudy/springbootgitstudy/SpringbootGitstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springstudy.springbootgitstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootGitstudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
